ab961da4e8ebbb00106c3f8f8e112385149ddd1e
Alejandro <alejandronicolasdelalamo@gmail.com>
2024-10-24 17:10:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const quien = ["el perro", "mi abuela", "su tortuga", "mi pájaro"];
+const que = ["se comió", "mojó", "aplastó", "rompió"];
+const cuando = [
+  "antes de la clase",
+  "justo a tiempo",
+  "cuando terminé",
+  "durante el almuerzo",
+  "mientras rezaba"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarExcusa() {
+  let parteQuien = quien[Math.floor(Math.random() * quien.length)];
+  let parteQue = que[Math.floor(Math.random() * que.length)];
+  let parteCuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+  return `${parteQuien} ${parteQue} mi tarea ${parteCuando}.`;
+}
+
+document.getElementById("generateBtn").addEventListener("click", function() {
+  let nuevaExcusa = generarExcusa();
+  document.getElementById("excuse").innerHTML = nuevaExcusa;
+});
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generarExcusa();
 };
